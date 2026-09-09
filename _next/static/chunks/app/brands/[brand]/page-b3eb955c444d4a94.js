46d4e7372c1a156cbb62289ddd3e0ecdce00327c
Chezihui Dev <dev@czhcar.com>
2026-09-09 16:25:38 +0800

    feat(site): replace internal quote links with customer inquiry CTAs

--- _next/static/chunks/app/brands/[brand]/page-b3eb955c444d4a94.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[726],{7231:function(e,n,s){Promise.resolve().then(s.bind(s,3020)),Promise.resolve().then(s.t.bind(s,2972,23))}},function(e){e.O(0,[972,302,904,964,971,117,744],function(){return e(e.s=7231)}),_N_E=e.O()}]);
